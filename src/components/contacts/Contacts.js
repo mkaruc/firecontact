c253f8da2a1327de72c75d3159f34b361f71e835
mkaruc <fatihmehmetkaruc@gmail.com>
2022-07-28 23:10:07 +0300

    almostfinished

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -9,10 +9,14 @@ import {
   Paper
 } from "@mui/material";
 
+import {useFetch,DeleteUser} from "../../utils/functions"
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 
-const Contacts = () => {
- 
+const Contacts = ({editUser}) => {
+ const {isLoading,contactList}=useFetch();
+
   return (
     <div>
       <h2 className="contact-header">Contacts</h2>
@@ -21,23 +25,56 @@ const Contacts = () => {
           <TableHead>
             <TableRow>
               <TableCell>Username</TableCell>
-              <TableCell align="right">Phone Number</TableCell>
-              <TableCell align="right">Gender</TableCell>
-              <TableCell align="right">Delete</TableCell>
-              <TableCell align="right">Edit</TableCell>
+              <TableCell align="left">Phone Number</TableCell>
+              <TableCell align="left">Gender</TableCell>
+              <TableCell align="left">Delete</TableCell>
+              <TableCell align="left">Edit</TableCell>
             </TableRow>
           </TableHead>        
          
-          <TableBody>           
-              <TableRow>
-              <TableCell textAlign="center"></TableCell>
-              <TableCell textAlign="center"></TableCell>
-              <TableCell textAlign="center"></TableCell> 
-              <TableCell textAlign="center">
-              </TableCell> 
-              <TableCell textAlign="center">
-              </TableCell> 
-             </TableRow>      
+          <TableBody>  
+          
+
+            {
+              // /* Bilgiler gelmediği durumda Loading yazısı görünsün */
+              isLoading ? (
+                <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>            
+                <TableCell colSpan={5} align="center">Loading</TableCell>             
+              </TableRow>
+              ) :
+              // Bilgiler olmadığı,boş olduğu  durumda veri bulunamadı mesajı
+                contactList?.length===0 ?(
+                  <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>            
+                  <TableCell colSpan={5} align="center">NO RESULT FOUND</TableCell>             
+                </TableRow>
+                  ) :
+                  // Bilgiler geldiği zaman aşağıya yazılacak kodlar çalışsın
+                  (
+                    contactList?.map((item,index)=>(
+
+                      <TableRow>
+                      <TableCell textAlign="center">{item.username.toUpperCase()}</TableCell>
+                      <TableCell textAlign="center">{item.phoneNumber}</TableCell>
+                      <TableCell textAlign="center">{item.gender}</TableCell> 
+                      <TableCell textAlign="center" onClick={()=>DeleteUser(item.id)}>
+                        <DeleteIcon/>
+                      </TableCell> 
+                      <TableCell textAlign="center"
+                     onClick={()=>editUser(
+                      item.id, item.username,item.phoneNumber,item.gender
+                    )}>
+                        <EditIcon/>
+                      </TableCell> 
+                     </TableRow>      
+
+                    ))
+                  )
+
+            }
+           
+              
+
+             
 
           </TableBody>
         </Table>
