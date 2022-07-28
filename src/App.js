78acab8fe67dcdd961d1d74621cb349f480ce5f3
mkaruc <fatihmehmetkaruc@gmail.com>
2022-07-28 21:45:29 +0300

    firebase

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Contacts from './components/contacts/Contacts';
+import FormComponent from './components/form/FormComponent';
+import {useState} from "react"
+import {AddUser} from "./utils/functions"
+
+const initialValues={username:"",phoneNumber:"",gender:"NO INFO"}
+
+function App() {
+  const [info,setInfo]=useState(initialValues)
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    AddUser(info)
+  }
+  return (
+    <div className="App">
+      <FormComponent info={info} setInfo={setInfo} handleSubmit={handleSubmit} />
+      <Contacts/>
+    </div>
+  );
+}
+
+export default App;
